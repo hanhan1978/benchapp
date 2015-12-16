94ebb5ee4b93f5bb952016099b52f304aeae08cf
hanhan1978 <ryo.tomidokoro@gmail.com>
2015-12-16 17:10:26 +0900

    Add some static files

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -1,6 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <link rel='stylesheet' href='/css/main.css'>
+    <script src='/js/jquery-2.1.4.min.js'></script>
+  </head>
+  <body>
+  <ul>
+  @foreach($todos as $todo)
+  <li>{{ $todo->content }}</li>
+  @endforeach
+  </ul>
+  <img src='/images/01.jpg' />
+  <img src='/images/02.jpg' />
+  <img src='/images/03.jpg' />
+  <img src='/images/04.jpg' />
+  <img src='/images/05.jpg' />
+  <img src='/images/06.jpg' />
+  <img src='/images/07.jpg' />
+  <img src='/images/08.jpg' />
+  <img src='/images/09.jpg' />
+  <img src='/images/10.jpg' />
+  <img src='/images/11.jpg' />
+  <img src='/images/12.jpg' />
+  </body>
 
-<ul>
-@foreach($todos as $todo)
-<li>{{ $todo->content }}</li>
-@endforeach
-</ul>
+</html>
